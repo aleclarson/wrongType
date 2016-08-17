b9b050a998402211388adeb0eb4a06efcf1f9aa1
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-08-17 16:49:43 -0700

    Throw when 'type' is not a kind of Object

--- js/wrongType.js
+++ js/wrongType.js
@@ -4,6 +4,9 @@ formatType = require("formatType");
 
 wrongType = function(type, key) {
   var reason;
+  if (!(type instanceof Object)) {
+    throw TypeError("'type' must be a constructor (or a validator)!");
+  }
   reason = key ? "'" + key + "' must be " : "Expected ";
   reason += formatType(type, true);
   return TypeError(reason + "!");
--- src/wrongType.coffee
+++ src/wrongType.coffee
@@ -2,6 +2,10 @@
 formatType = require "formatType"
 
 wrongType = (type, key) ->
+
+  unless type instanceof Object
+    throw TypeError "'type' must be a constructor (or a validator)!"
+
   reason = if key then "'#{key}' must be " else "Expected "
   reason += formatType type, yes
   return TypeError reason + "!"
